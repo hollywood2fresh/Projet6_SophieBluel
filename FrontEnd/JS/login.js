03f8b0523778d4228146e69ef667fc61e2b75165
hollywood2fresh <mael.boilevepro@gmail.com>
2023-03-23 11:04:59 +0100

    login

---
+++ FrontEnd/JS/login.js
@@ -0,0 +1,95 @@
+const loginForm = document.getElementById('login');
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('jai clicker');
+  let baliseEmail = document.getElementById('email');
+  let baliseMotDePasse = document.getElementById('password');
+  let email = baliseEmail.value;
+  let motDePasse = baliseMotDePasse.value;
+  console.log(email, motDePasse);
+login(email, motDePasse)
+})
+
+
+async function login(email, motDePasse) {
+  let user = {
+    email: email,
+    password: motDePasse
+  };
+  console.log(user);
+  let response = await fetch('http://localhost:5678/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  }); 
+  
+  let result = await response.json();
+  console.log(result);
+}
+
+
+
+
+
+// document.getElementById('login').addEventListener('submit', function(e) { 
+    
+
+//     let erreur;
+
+//     if(erreur) {
+//         e.preventDefault(); // empecher le comportement par default du formulaire 
+//         document.getElementById('erreur').innerHTML = erreur;
+//         return false;
+//     } else {
+//         alert('formulaire envoyé !!')
+//     }
+
+//     // let email = document.getElementById('email');
+//     // let motDePasse = document.getElementById('password');
+
+    
+//     // if(!motDePasse.value) {
+//     //     erreur = "Veuillez renseigner votre mot de passe";
+//     // }
+//     // if(!email.value) {
+//     //     erreur = "Veuillez renseigner votre e-mail";
+//     // }
+    
+   
+// })
+
+// let users = {
+//     name: 
+// }
+
+// async function postData(url = '', data = {}) {
+//     const response = await fetch('http://localhost:5678/api/users', {
+//         method: 'POST'
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//         body: JSON.stringify(data)
+//     });
+//     return response.json();
+// }
+
+// let user = {
+//     name: 'John',
+//     surname: 'Smith'
+//   };
+  
+//   let response = await fetch('http://localhost:5678/api/users/login', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(user)
+//   });
+  
+//   let result = await response.json();
+//   alert(result.message);
+
+//   console.log(user);
